b19959bf06c6047b8c4ea7a67e39047025b71cc6
Robert Brinckman <robert.brinckman@kellogg.northwestern.edu>
2016-12-02 13:45:12 -0600

    4th submit

--- src/index.js
+++ src/index.js
@@ -32,33 +32,47 @@ window.getDeck = function() {
 }
 // END OF STUFF TO NOT MODIFY
 
+
+var Card = React.createClass({
+ render: function (){
+   return (
+     <div className="col-sm-2">
+       <img className="img-responsive" alt="cards" src={"http://golearntocode.com/images/cards/" + this.props.name +".png"}/>
+     </div>
+     )
+ }
+})
+
 var App = React.createClass({
-  deal: function() {
-    var deck = window.getDeck().shuffle()
-    var newHand = deck.splice(0,5)
-    this.setState({
-      hand: newHand
-    })
-},
+ deal: function(){
+     var deck = window.getDeck().shuffle()
+     var newHand = [deck[0], deck[1], deck[2], deck[3], deck[4]]
+     this.setState({
+       hand: newHand
+     })
+ },
+ getInitialState: function(){
+   return {
+       hand: ["face_down","face_down","face_down","face_down","face_down"]
+   }
+ },
+ render: function() {
+   return (
+     <div>
+       <h1>Welcome to the KIEI-924 Casino!</h1>
+       <div className="row">
+         <Card name={this.state.hand[0]}/>
+         <Card name={this.state.hand[1]}/>
+         <Card name={this.state.hand[2]}/>
+         <Card name={this.state.hand[3]}/>
+         <Card name={this.state.hand[4]}/>
+       </div>
+       <div className="col-sm-2">
+         <div><a href="#" className="btn btn-success" onClick={this.deal} >Deal</a></div>
+       </div>
+     </div>
+   )
+ }
+})
 
-  getInitialState: function() {
-    return {
-      hand: ["face_down","face_down","face_down","face_down","face_down"]
-    }
-  },
-
-  render: function() {
-    let boundClick = this.deal.hand;
-    return (
-      <div>
-        <h1>Welcome to KIEI-924 Casino!</h1>
-        <div className="row">
-          <Card name={this.state.hand[0]} />
-          <Card name={this.state.hand[1]} />
-          <Card name={this.state.hand[2]} />
-          <Card name={this.state.hand[3]} />
-          <Card name={this.state.hand[4]} />
-    )
-  }
-}
 ReactDOM.render(<App />, document.getElementById("app"))
